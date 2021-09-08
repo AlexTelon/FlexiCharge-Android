a40c47ae91538f64064677d0aa1404acc2f4fd72
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2021-09-08 15:55:07 +0200

    Input dialog & improved input navigation

--- app/src/main/java/com/flexicharge/bolt/MainActivity.kt
+++ app/src/main/java/com/flexicharge/bolt/MainActivity.kt
@@ -2,10 +2,63 @@ package com.flexicharge.bolt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_charger_input)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            setupChargerInput()
+        }
+
+    }
+    private fun setupChargerInput() {
+
+        val bottomSheetDialog = BottomSheetDialog(
+            this@MainActivity, R.style.BottomSheetDialogTheme
+        )
+        val bottomSheetView = LayoutInflater.from(applicationContext).inflate(
+            R.layout.layout_charger_input,
+            findViewById<ConstraintLayout>(R.id.chargerInputLayout)
+        )
+
+        setupChargerInputFocus(bottomSheetView)
+
+        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.show()
+    }
+
+    private fun setupChargerInputFocus(view: View) {
+        val editTextInput1 = view.findViewById<EditText>(R.id.charger_input_edit_text_1)
+        val editTextInput2 = view.findViewById<EditText>(R.id.charger_input_edit_text_2)
+        val editTextInput3 = view.findViewById<EditText>(R.id.charger_input_edit_text_3)
+        val editTextInput4 = view.findViewById<EditText>(R.id.charger_input_edit_text_4)
+        val editTextInput5 = view.findViewById<EditText>(R.id.charger_input_edit_text_5)
+        val editTextInput6 = view.findViewById<EditText>(R.id.charger_input_edit_text_6)
+
+        editTextInput1.doOnTextChanged { _, _, _, count ->
+            if (count == 1) editTextInput2.requestFocus()
+        }
+        editTextInput2.doOnTextChanged { _, _, _, count ->
+            if (count == 1) editTextInput3.requestFocus()
+        }
+        editTextInput3.doOnTextChanged { _, _, _, count ->
+            if (count == 1) editTextInput4.requestFocus()
+        }
+        editTextInput4.doOnTextChanged { _, _, _, count ->
+            if (count == 1) editTextInput5.requestFocus()
+        }
+        editTextInput5.doOnTextChanged { _, _, _, count ->
+            if (count == 1) editTextInput6.requestFocus()
+        }
     }
 }
