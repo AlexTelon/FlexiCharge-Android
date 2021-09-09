21c9c75d94b48dae7000a012a5d40c1c7dc5efd5
runil <57109651+runil@users.noreply.github.com>
2021-09-09 08:43:08 +0200

    Sending path paremeter to API from GUI working

--- app/src/main/java/com/flexicharge/bolt/MainActivity.kt
+++ app/src/main/java/com/flexicharge/bolt/MainActivity.kt
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.widget.doOnTextChanged
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import android.util.Log
-import android.widget.Toast
+import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
 import com.flexicharge.bolt.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
@@ -45,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         )
 
         setupChargerInputFocus(bottomSheetView)
-
+        setupChargerIdValidator(bottomSheetView)
         bottomSheetDialog.setContentView(bottomSheetView)
         bottomSheetDialog.show()
     }
@@ -75,11 +74,58 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun validateConnectionToMockDataApi() {
+    private fun setupChargerIdValidator(view: View) {
+        val editTextInput1 = view.findViewById<EditText>(R.id.charger_input_edit_text_1)
+        val editTextInput2 = view.findViewById<EditText>(R.id.charger_input_edit_text_2)
+        val editTextInput3 = view.findViewById<EditText>(R.id.charger_input_edit_text_3)
+        val editTextInput4 = view.findViewById<EditText>(R.id.charger_input_edit_text_4)
+        val editTextInput5 = view.findViewById<EditText>(R.id.charger_input_edit_text_5)
+        val editTextInput6 = view.findViewById<EditText>(R.id.charger_input_edit_text_6)
+        val chargerInputStatus = view.findViewById<TextView>(R.id.charger_input_status)
+        editTextInput6.doOnTextChanged { _, _, _, count ->
+            //if (count == 1) editTextInput6.requestFocus()
+            var chargerId = (editTextInput1.text.toString() +
+                    editTextInput2.text.toString() +
+                    editTextInput3.text.toString() +
+                    editTextInput4.text.toString() +
+                    editTextInput5.text.toString() +
+                    editTextInput6.text.toString())
+            chargerInputStatus.text = "Connected to " + chargerId
+            chargerInputStatus.setBackgroundResource(R.color.green)
+            //chargerInputStatus.setTextColor(resources.getColor(R.color.green))
+            validateConnectionToMockDataApi(chargerId)
+            Log.d("editTextInput6",chargerId)
+        }
+    }
+
+
+
+/*    fun validateChargerIdInput(
+        chargerId: String
+    ): Boolean {
+        if(chargerId.length != 6) {
+            return false
+        }
+        if(chargerId.count { it.isDigit() } != 6) {
+            return false
+        }
+        if(chargerId in occupiedChargerIds) {
+            return false
+        }
+        if(chargerId !in availableChargerIds) {
+            return false
+        }
+
+        return true
+    }*/
+
+
+
+    private fun validateConnectionToMockDataApi(chargerId: String) {
         lifecycleScope.launch(Dispatchers.IO) {
             try {
 
-                val response = RetrofitInstance.api.getMockApiData()
+                val response = RetrofitInstance.api.getMockApiData(chargerId)
                 Log.d("asdf", response.isSuccessful.toString())
                 if (response.isSuccessful) {
                     val chargerId = response.body() as FakeJsonResponse
