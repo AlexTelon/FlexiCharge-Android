0f88e3e2571b19de387e31bae22f35a3ae02399f
runil <57109651+runil@users.noreply.github.com>
2021-09-14 11:56:09 +0200

    Created and designed RegisterSplashActivity

---
+++ app/src/main/java/com/flexicharge/bolt/RegisterSplashActivity.kt
@@ -0,0 +1,31 @@
+package com.flexicharge.bolt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class RegisterSplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_splash)
+    }
+
+    fun confirmRegistration(view: View) {
+        // Validate user input
+        // Check TOS Agreement
+        // Communicate with backend
+        // Handle Backend Reply
+        // Proceed to MainActivity upon confirmation
+        val text = "Hello toast!"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
+    }
+    fun goToSignIn(view: View) {
+        //Go to sign in activity
+    }
+    fun continueAsGuest(view: View) {
+        //Continue to MainActivity
+    }
+}
