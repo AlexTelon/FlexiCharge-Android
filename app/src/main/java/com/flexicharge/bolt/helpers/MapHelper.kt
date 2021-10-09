3c31446b85debfbb72cb3156585dea6a12858f53
runil <57109651+runil@users.noreply.github.com>
2021-10-09 11:13:11 +0200

    Added permissionactivity and created singleton for map related code

---
+++ app/src/main/java/com/flexicharge/bolt/helpers/MapHelper.kt
@@ -0,0 +1,116 @@
+package com.flexicharge.bolt.helpers
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.flexicharge.bolt.api.flexicharge.Chargers
+import com.flexicharge.bolt.activities.MainActivity
+import com.flexicharge.bolt.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.Exception
+
+object MapHelper {
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var currentLocation: Location
+    lateinit var mMap: GoogleMap
+    const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    const val PERMISSION_CODE = 101
+
+    fun currLocation(activity: MainActivity){
+        if (MapHelper::currentLocation.isInitialized) {
+            fetchLocation(activity)
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(currentLocation.latitude, currentLocation.longitude), 13f))
+        } else {
+            Toast.makeText(activity, "Location permissions are required for this feature.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun fetchLocation(activity: MainActivity) {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
+        if (ActivityCompat.checkSelfPermission(
+                activity, Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                activity, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_CODE
+            )
+            return
+        }
+        val task = fusedLocationProviderClient.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                val supportMapFragment =
+                    activity.supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                supportMapFragment.getMapAsync(activity)
+                getLocationAccess(activity)
+                setCurrentLocation(activity)
+            }
+        }
+    }
+
+    fun onRequestPermissionsResult(activity: MainActivity, requestCode: Int, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_CODE ->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocationAccess(activity)
+                }
+        }
+    }
+
+    fun setCurrentLocation(activity: MainActivity) {
+        try {
+            val curPos = LatLng(currentLocation.latitude, currentLocation.longitude)
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curPos, 13f))
+        } catch (e: Exception) {
+            Toast.makeText(activity,"Location permissions are required for MyLocation.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun getLocationAccess(activity: MainActivity) {
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled = true
+        }
+        else
+            ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+    }
+
+    fun addNewMarkers(activity: MainActivity, chargers: Chargers){
+        val blackIcon = BitmapDescriptorFactory.fromBitmap(activity.getDrawable(R.drawable.ic_black_marker)?.toBitmap())
+        val greenIcon = BitmapDescriptorFactory.fromBitmap(activity.getDrawable(R.drawable.ic_green_marker)?.toBitmap())
+        val redIcon = BitmapDescriptorFactory.fromBitmap(activity.getDrawable(R.drawable.ic_red_marker)?.toBitmap())
+        chargers.forEach {
+            val marker = mMap.addMarker(MarkerOptions().position(LatLng(it.location[0], it.location[1])).title(it.chargerID.toString()))
+            if(it.status == "Available") marker.setIcon(greenIcon)
+            else marker.setIcon(redIcon)
+        }
+    }
+
+    fun panToPos (
+        latitude: Double,
+        longitude: Double,
+    ) {
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 13f))
+    }
+
+    fun onMapReady(activity: MainActivity, googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isMyLocationButtonEnabled = false
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(activity, R.raw.flexicharge_map_style) )
+    }
+}
