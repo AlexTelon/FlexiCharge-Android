d90819add974ea5df5538fc45cf12b6c1d27d634
otpe20pu <81689565+otpe20pu@users.noreply.github.com>
2022-09-14 12:29:03 +0200

    Added 10 second timeout for the API tests

--- app/src/androidTest/java/com/flexicharge/bolt/TestApi.kt
+++ app/src/androidTest/java/com/flexicharge/bolt/TestApi.kt
@@ -4,9 +4,13 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import com.flexicharge.bolt.api.flexicharge.RetrofitInstance
 import kotlinx.coroutines.runBlocking
+import org.junit.Rule
+import org.junit.rules.Timeout
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class TestApi {
+    @get:Rule public var timeout = Timeout(10, TimeUnit.SECONDS)
 
     @Test
     fun testGetChargerSuccessful() = runBlocking {
