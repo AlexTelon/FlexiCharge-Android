3f3bb80eeb4b2b4676cdee643d5ae2eb43626c28
rezaMor2021 <81688626+rezaMor2021@users.noreply.github.com>
2022-10-04 18:41:33 +0200

    removed button from mainActivity

--- app/src/main/java/com/flexicharge/bolt/activities/MainActivity.kt
+++ app/src/main/java/com/flexicharge/bolt/activities/MainActivity.kt
@@ -92,12 +92,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ChargePointListAda
 
         }
 
-        ///---------------------------------------------------------test----------------------------------------------------
-        binding.recoverpaswordTest.setOnClickListener{
-            val intent = Intent(this, ForgotPasswordActivity::class.java)
-            startActivity(intent)
-        }
-     //----------------------------------------------------------------------------------------------------------------
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.mainActivity_fragment_map) as SupportMapFragment
