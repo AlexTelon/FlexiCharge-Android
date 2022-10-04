b03e6440dbbd7d7d47e530b2a80d41c24ae942bd
rezaMor2021 <81688626+rezaMor2021@users.noreply.github.com>
2022-10-04 17:00:00 +0200

    Added FragmentRecoverPass
    Get email from Edittext and pass it to FragmentrecoverEmailSent

--- app/src/main/java/ForgetPassFragment/FragmentRecoverPass.kt
+++ app/src/main/java/ForgetPassFragment/FragmentRecoverPass.kt
@@ -1,51 +1,66 @@
-package ForgetPassFragment
+package FragmentRecoverPass
+
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Space
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.flexicharge.bolt.Communicator_
-import com.flexicharge.bolt.R
-
-import com.google.android.gms.tasks.OnCanceledListener
-
-
+import com.flexicharge.bolt.api.flexicharge.RetrofitInstance
+import com.flexicharge.bolt.databinding.FragmentRecoverPassBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 class FragmentRecoverPass : Fragment() {
-
-    private  lateinit var communicator: Communicator_
+    private lateinit var communicator: Communicator_
+    private var _binding: FragmentRecoverPassBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        _binding = FragmentRecoverPassBinding.inflate(inflater, container, false)
+        val email = binding.resetActivityEditTextEmail.text.toString()
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_recover_pass, container, false)
-
         communicator = activity as Communicator_
-        view.findViewById<Button>(R.id.buttonConfirmRecoverPassword).setOnClickListener{
-            val email: String = view.findViewById<EditText>(R.id.loginActivity_editText_email).text.toString().trim{it <= ' '}
-            fun String.isEmailValid(): Boolean {
-                return !email.isEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
-            }
-            if (!email.isEmailValid()){
-                Toast.makeText(activity, "The email address is badly formatted", Toast.LENGTH_SHORT).show()
-            }else{
 
-                //here check the address
+        binding.buttonConfirmRecoverPassword.setOnClickListener {
 
+                sendEmailAdd()
+        }
+        return binding.root
+    }
+        private fun sendEmailAdd() {
+            val email = binding.resetActivityEditTextEmail.text.toString()
+            lifecycleScope.launch(Dispatchers.Main) {
+                try {
+                    val response = RetrofitInstance.flexiChargeApi.resetPass(email)
 
+                    if (response.code() == 200) {
+                        // navigate and pass Email address to fragmentrecoverEnamiSent
+                        communicator.passDataCom(binding.resetActivityEditTextEmail.text.toString())
+                    } else if (response.code() == 400) {
+                        Toast.makeText(activity,  "Attempt limit exceeded, please try later.", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: HttpException) {
+                    Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
+                } catch (e: IOException) {
+                    Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
 
-                communicator.passDataCom(view.findViewById<EditText>(R.id.loginActivity_editText_email).text.toString())
-
+                }
             }
         }
-        return view
+
 
     }
 
-}
+
+
+
+
