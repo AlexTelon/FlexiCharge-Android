74cc64b035bf5d606078a8b6ec0e25280d7cb932
DavidSamueIsson <david.s9712@gmail.com>
2023-09-22 00:08:06 +0200

    Added mockdata and created the correct UI for displaying

--- app/src/main/java/com/flexicharge/bolt/activities/ChargingHistoryActivity.kt
+++ app/src/main/java/com/flexicharge/bolt/activities/ChargingHistoryActivity.kt
@@ -6,10 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.flexicharge.bolt.R
-import com.flexicharge.bolt.adapters.ChargePointListAdapter
-import com.flexicharge.bolt.adapters.InvoiceAdapter
-import com.flexicharge.bolt.databinding.ActivityAccountSettingsBinding
-
+import com.flexicharge.bolt.adapters.ChargeHistoryAdapter
+import com.flexicharge.bolt.api.flexicharge.ChargingHistoryObject
 import com.flexicharge.bolt.databinding.ActivityChargingHistoryBinding
 
 class ChargingHistoryActivity : AppCompatActivity() {
@@ -20,11 +18,71 @@ class ChargingHistoryActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val recyclerView : RecyclerView = findViewById(R.id.chargingHistory_recyclerView)
-        val data = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12")
 
-        val adapter = InvoiceAdapter(data)
+        val data = createMockData()
+        val adapter = ChargeHistoryAdapter(data)
 
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun createMockData() : List<ChargingHistoryObject>{
+
+        val charge1 = ChargingHistoryObject(
+            location        = "Per-brahe parkeringshus, Jönköping",
+            totalSum        = "27.74",
+            startTime       = "15 June, kl 12:04",
+            chargeTime      = "3 hrs 54 min",
+            transferedKwh   = "9.24",
+            priceKwh        = "3.00"
+        )
+
+        val charge2 = ChargingHistoryObject(
+            location        = "Per-brahe parkeringshus, Jönköping",
+            totalSum        = "27.74",
+            startTime       = "15 June, kl 12:04",
+            chargeTime      = "3 hrs 54 min",
+            transferedKwh   = "9.24",
+            priceKwh        = "3.00"
+        )
+
+        val charge3 = ChargingHistoryObject(
+            location        = "Per-brahe parkeringshus, Jönköping",
+            totalSum        = "27.74",
+            startTime       = "18 June, kl 14:04",
+            chargeTime      = "4 hrs 23 min",
+            transferedKwh   = "10.58",
+            priceKwh        = "2.48"
+        )
+
+        val charge4 = ChargingHistoryObject(
+            location        = "Per-brahe parkeringshus, Jönköping",
+            totalSum        = "27.74",
+            startTime       = "15 June, kl 12:04",
+            chargeTime      = "3 hrs 54 min",
+            transferedKwh   = "9.24",
+            priceKwh        = "3.00"
+        )
+
+        val charge5 = ChargingHistoryObject(
+            location        = "Per-brahe parkeringshus, Jönköping",
+            totalSum        = "27.74",
+            startTime       = "15 June, kl 12:04",
+            chargeTime      = "3 hrs 54 min",
+            transferedKwh   = "9.24",
+            priceKwh        = "3.00"
+        )
+
+        val charge6 = ChargingHistoryObject(
+            location        = "Per-brahe parkeringshus, Jönköping",
+            totalSum        = "27.74",
+            startTime       = "15 June, kl 12:04",
+            chargeTime      = "3 hrs 54 min",
+            transferedKwh   = "9.24",
+            priceKwh        = "3.00"
+        )
+
+        return  listOf(charge1,charge2,charge3,charge4,charge5,charge6)
+
     }
 }
