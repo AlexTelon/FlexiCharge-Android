14f6154218c074d2229e104612acd2de9ce54582
Talal <92815664+TGH97@users.noreply.github.com>
2023-10-12 12:03:41 +0200

    deleted duplicated tests

--- app/src/test/java/com/flexicharge/bolt/ExampleUnitTest.kt
+++ app/src/test/java/com/flexicharge/bolt/ExampleUnitTest.kt
@@ -37,55 +37,9 @@ class ExampleUnitTest {
         assert(response.isSuccessful)
     }
 
-    @Test
+   /* @Test
     fun testGetChargePoint() = runBlocking {
         val response = RetrofitInstance.flexiChargeApi.getChargePoint(100029)
         assert(response.isSuccessful)
-    }
-
-    @Test
-    fun testLogin() = runBlocking {
-        val email = "didiwa6692@searpen.com"
-        val pass = "Test1234!"
-        val credentials = Credentials(email, pass)
-        val response = RetrofitInstance.flexiChargeApi.signIn(credentials)
-
-        assert(response.isSuccessful)
-    }
-
-    @Test
-    fun getUserInfo() = runBlocking {
-        val email = "donene8581@vip4e.com"
-        val pass = "Test123!"
-        val credentials = Credentials(email, pass)
-        val response = RetrofitInstance.flexiChargeApi.signIn(credentials)
-        val token = response.body()?.accessToken
-        assert(response.isSuccessful)
-
-        val info = RetrofitInstance.flexiChargeApi.getUserInfo("Bearer $token")
-        println(info)
-        assert(info.isSuccessful)
-    }
-
-    @Test
-    fun updateUserInfo() = runBlocking {
-        val email = "powamat696@vip4e.com"
-        val pass = "Test123!"
-        val credentials = Credentials(email, pass)
-        val login = RetrofitInstance.flexiChargeApi.signIn(credentials)
-        val token = login.body()?.accessToken
-        assert(login.isSuccessful)
-
-        val userInfo = UserFullDetails(
-            "test33",
-            "Testarsson",
-            "",
-            "",
-            "",
-            "",
-            ""
-        )
-        val update = RetrofitInstance.flexiChargeApi.updateUserInfo("Bearer $token", userInfo)
-        assert(update.isSuccessful)
-    }
+    }*/
 }
